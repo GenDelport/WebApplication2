34ae997ee82df197f0d0be874ed54fa315820f6d
Cloete <cloetedelport14@gmail.com>
2021-11-19 15:46:39 +0200

    new

---
+++ wwwroot/js/create.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $('#btnSaveCheckBox').click(function () {
+        addCheckbox($('#checkBoxName').val());
+        $('#checkBoxName').val("");
+    });
+
+    $(document).on('change', 'input[type="checkbox"]', updateProgress);
+
+    $("#progressbar").progressbar({
+        value: 0,
+        max: 100
+    });
+});
+
+function updateProgress() {
+    var numAll = $('input[type="checkbox"]').length;
+    var numChecked = $('input[type="checkbox"]:checked').length;
+
+    if (numAll > 0) {
+        var perc = (numChecked / numAll) * 100;
+        $("#progressbar").progressbar("value", perc);
+    }
+}
+
+function addCheckbox(name) {
+    var container = $('#cblist');
+    var inputs = container.find('input');
+    var id = inputs.length + 1;
+
+    $('<input />', { type: 'checkbox', id: 'cb' + id, value: name }).appendTo(container);
+    $('<label />', { 'for': 'cb' + id, text: name }).appendTo(container);
+    $('<br/>').appendTo(container);
+
+    updateProgress();
+}
