9b7b6ea47286ad24439a2c275a8e4c63ea8279fd
Cloete <cloetedelport14@gmail.com>
2021-11-28 13:19:49 +0200

    updated

---
+++ wwwroot/js/examiner.js
@@ -0,0 +1,20 @@
+﻿function addExaminer() {
+    var number = document.getElementById('inputNumber').value;
+
+    var surname = document.getElementById('inputSurname').value;
+
+    var initials = document.getElementById('inputInitials').value;
+
+    var title = document.querySelector('#inputTitle').value;
+
+    var result = number + ', ' + surname + ', ' + initials + ', ' + title;
+
+    document.getElementById('output').textContent = result;
+
+    /*var output = document.getElementsByClassName('output');*/
+
+    //output.appendChild(number);
+    //output.appendChild(surname);
+    //output.appendChild(initials);
+    //output.appendChild(title);
+}
